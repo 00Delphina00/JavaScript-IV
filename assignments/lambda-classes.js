2a7852a36cd9d9be425136af17a535e083f2175f
Delphina Bailey Nicols <DelphinaBaileyNicols@gmail.com>
2019-04-25 20:50:01 -0400

    lambda-classes.js  Initial commit.  the Person base class works.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,25 @@
 // CODE here for your Lambda Classes
+//Base Class
+class Person {
+    constructor(newObjects){
+        this.name = newObjects.name;
+        this.age = newObjects.age;
+        this.location = newObjects.location;
+        this.gender = newObjects.gender;
+    };
+    speak(){
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
+    };
+}
+
+const fred = new Person({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+
+  fred.speak();
